6a24247624e00147f495c1e24c7ca99ce09cb438
吴伋 <wuji@gs-robot.com>
2019-10-10 10:48:21 +0800

    unittest

--- p1051-unittest.py
+++ p1051-unittest.py
@@ -1,3 +1,5 @@
+# Use assertIn() to test container membership.
+
 import unittest
 
 
---
+++ p1060-unittest.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class ContainerMemebershipTest(unittest.TestCase):
+    def testDict(self):
+        self.assertIn(4, {1: 'a', 2: 'b', 3: 'c'})
+
+    def testList(self):
+        self.assertIn(4, [1, 2, 3])
+
+    def testSet(self):
+        self.assertIn(4, set([1, 2, 3]))
